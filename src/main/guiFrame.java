63808715a8680e8600f7b8a62b6733617d8b436e
nikshinde1996 <crenickshinde1996@gmail.com>
2017-01-16 18:32:03 +0530

    all components added

--- src/main/guiFrame.java
+++ src/main/guiFrame.java
@@ -13,10 +13,11 @@ public class guiFrame extends JFrame{
 	public static JTextArea toEncodeText,encodedText;
 	public static JComboBox encoding,alignment,labelLocation;
 	public static String[] encodingTypes = {" Nikhil "," Swapnil "," Subham "," Kuldeep "," Abhishek "};
-	public static String[] alignmentTypes = {" Nikhil "," Swapnil "," Subham "," Kuldeep "," Abhishek "};
-	public static String[] labelLocationsTypes = {" Nikhil "," Swapnil "," Subham "," Kuldeep "," Abhishek "};
+	public static String[] alignmentTypes = {" Left "," Right "," Top "," Bottom "," Center "};
+	public static String[] labelLocationsTypes = {" Left "," Right "," Top "," Bottom "," Center "};
 	public static ColorChooserButton fcolor,bcolor;
 	public static JButton encode,saveAs,savexml;
+	public static JScrollPane toEncodeSPane,encodedSPane;
 	public static JCheckBox label;
 	public static JTextField labelText;
 	
@@ -30,8 +31,9 @@ public class guiFrame extends JFrame{
 		setSize(new Dimension(FRAME_WIDTH,FRAME_HEIGHT));
 		setLayout(new GridBagLayout());
 		
-		add(barcodePanel,new GBC(0,0).setWeight(1,0.4).setFill(GBC.BOTH));
-		add(configPanel,new GBC(0,1).setWeight(1,0.6).setFill(GBC.BOTH));
+		add(barcodePanel,new GBC(0,0).setWeight(1,0.8).setFill(GBC.BOTH));
+		add(configPanel,new GBC(0,1).setWeight(1,0.2).setFill(GBC.BOTH));
+
 	}
 	
 	public static void populateConfigPanel(){
@@ -39,33 +41,43 @@ public class guiFrame extends JFrame{
 		configPanel.setBorder(BorderFactory.createTitledBorder("  Barcode Image  "));
 		configPanel.setLayout(new GridBagLayout());
 	    
-		configPanel.add(new JLabel("Value to Encode"),new GBC(0,0,2,1));
-		configPanel.add(new JLabel("Encoded Value"),new GBC(3,0,2,1));
-		configPanel.add(new JScrollPane(toEncodeText = new JTextArea(30,200),JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER),new GBC(0,1,2,1));
-		configPanel.add(new JScrollPane(encodedText = new JTextArea(3,20),JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER),new GBC(3,1,2,1));
+		configPanel.add(new JLabel("Value to Encode"),new GBC(0,0,2,1).setInsets(0,0,3,120));
+		configPanel.add(new JLabel("Encoded Value"),new GBC(2,0,2,1).setInsets(0,0,3,120));
+		configPanel.add(toEncodeSPane=new JScrollPane(toEncodeText = new JTextArea(3,20),JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER),new GBC(0,1,2,1).setInsets(0,0,3,5));
+		configPanel.add(encodedSPane=new JScrollPane(encodedText = new JTextArea(3,20),JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER),new GBC(2,1,2,1));
 	    
-//		toEncodeText.setPreferredSize(preferredSize);
-		
-		configPanel.add(new JLabel("Encoding"),new GBC(0,2));
+    	toEncodeSPane.setPreferredSize(new Dimension((int)(FRAME_WIDTH*0.45),(int)(FRAME_HEIGHT*0.2)));
+    	encodedSPane.setPreferredSize(new Dimension((int)(FRAME_WIDTH*0.45),(int)(FRAME_HEIGHT*0.2)));
+    		
+		configPanel.add(new JLabel("Encoding"),new GBC(0,2).setInsets(5,0,2,50));
 	    configPanel.add(encoding = new JComboBox(encodingTypes),new GBC(0,3));
-	    configPanel.add(new JLabel("ForeGround"),new GBC(1,2));
+	    encoding.setPreferredSize(new Dimension(100,25));
+	    configPanel.add(new JLabel("ForeGround"),new GBC(1,2).setInsets(5,0,2,10));
 	    configPanel.add(fcolor = new ColorChooserButton(Color.BLACK),new GBC(1,3));
+	    fcolor.setPreferredSize(new Dimension(80,25));
 	    
-	    configPanel.add(new JLabel("Alignment"),new GBC(0,4));
-	    configPanel.add(encoding = new JComboBox(encodingTypes),new GBC(0,5));
-	    configPanel.add(new JLabel("BackGround"),new GBC(1,4));
+	    configPanel.add(new JLabel("Alignment"),new GBC(0,4).setInsets(10,0,2,45));
+	    configPanel.add(alignment = new JComboBox(alignmentTypes),new GBC(0,5));
+	    alignment.setPreferredSize(new Dimension(100,25));
+	    configPanel.add(new JLabel("BackGround"),new GBC(1,4).setInsets(10,0,2,10));
 	    configPanel.add(bcolor = new ColorChooserButton(Color.WHITE),new GBC(1,5));
+	    bcolor.setPreferredSize(new Dimension(80,25));
 	    
+	    configPanel.add(encode = new JButton(" Encode "),new GBC(0,6).setInsets(15,7,0,10));
+	    encode.setPreferredSize(new Dimension(100,25));
 	    
-	    configPanel.add(encode = new JButton(" Encode "),new GBC(0,6));
-	    configPanel.add(saveAs = new JButton(" Save As "),new GBC(1,6));
+	    configPanel.add(saveAs = new JButton("Save As"),new GBC(1,6).setInsets(15,0,0,0));
+	    saveAs.setPreferredSize(new Dimension(80,25));
 	    
-	    configPanel.add(new JLabel("Alternate Label Text"),new GBC(2,2,2,1));
-	    configPanel.add(labelText = new JTextField(10),new GBC(2,3,2,1));
-	    configPanel.add(new JLabel("Label location"),new GBC(2,4,2,1));
+	    configPanel.add(new JLabel("Alternate Label Text"),new GBC(2,2,2,1).setInsets(5,0,2,90));
+	    configPanel.add(labelText = new JTextField(18),new GBC(2,3,2,1));
+	    configPanel.add(new JLabel("Label location"),new GBC(2,4,2,1).setInsets(10,0,2,120));
 	    configPanel.add(labelLocation = new JComboBox(labelLocationsTypes),new GBC(2,5,2,1));
-	    configPanel.add(savexml = new JButton("Save XML"),new GBC(2,6));
-	    configPanel.add(label = new JCheckBox("Generate Label"),new GBC(3,6));
+	    labelLocation.setPreferredSize(new Dimension(200,25));
+	    
+	    configPanel.add(savexml = new JButton("SaveXML"),new GBC(2,6).setInsets(15,0,0,0));
+	    savexml.setPreferredSize(new Dimension(80,25));
+	    configPanel.add(label = new JCheckBox("Generate Label"),new GBC(3,6).setInsets(15,0,0,0));
 		
 	}
 	
